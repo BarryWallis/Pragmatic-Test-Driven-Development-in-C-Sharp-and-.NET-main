0f40e914e571a238e76c49986a369a8d13264317
Barry Wallis <barry.wallis@outlook.com>
2022-12-28 12:45:52 -0800

    Chapter 2: Initial weather forecast API.

---
+++ UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uqs.Weather.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] _summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+#pragma warning disable IDE0052 // Remove unread private members
+    private readonly ILogger<WeatherForecastController> _logger;
+#pragma warning restore IDE0052 // Remove unread private members
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger) => _logger = logger;
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get() => Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = _summaries[Random.Shared.Next(_summaries.Length)]
+    })
+        .ToArray();
+}
