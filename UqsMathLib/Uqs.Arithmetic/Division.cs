2d3b1e3b8bfa85580b2e94e89d33c4baa729f057
Barry Wallis <barry.wallis@outlook.com>
2022-12-27 18:55:13 -0800

    Chapter 1 completed.

---
+++ UqsMathLib/Uqs.Arithmetic/Division.cs
@@ -0,0 +1,19 @@
+﻿namespace Uqs.Arithmetic;
+
+/// <summary>
+/// Class to process divisions.
+/// </summary>
+public class Division
+{
+    /// <summary>
+    /// Divide the <paramref name="dividend"/> by the <paramref name="divisor"/> and return the quotient.
+    /// </summary>
+    /// <param name="dividend">The dividend.</param>
+    /// <param name="divisor">The divisor.</param>
+    /// <returns>The quotient.</returns>
+    public static decimal Divide(int dividend, int divisor)
+    {
+        decimal quotient = (decimal)dividend / divisor;
+        return quotient;
+    }
+}
---
+++ UqsMathLib/Uqs.Artithmetic.Tests.Unit/DivisionTests.cs
@@ -0,0 +1,54 @@
+﻿using Uqs.Arithmetic;
+
+namespace Uqs.Artithmetic.Tests.Unit;
+
+public class DivisionTests
+{
+    [Fact]
+    public void Divide_DivisibleIntegers_WholeNumber()
+    {
+        int dividend = 10;
+        int divisor = 5;
+        decimal expectedQuotient = 2.00M;
+
+        decimal actualQuotient = Division.Divide(dividend, divisor);
+
+        Assert.Equal(expectedQuotient, actualQuotient);
+    }
+
+    [Fact]
+    public void Divide_IndivisibleIntegers_DecimalNumber()
+    {
+        int dividend = 10;
+        int divisor = 4;
+        decimal expectedQuotient = 2.50M;
+
+        decimal actualQuotient = Division.Divide(dividend, divisor);
+
+        Assert.Equal(expectedQuotient, actualQuotient);
+    }
+
+    [Fact]
+    public void Divide_ZeroDivisor_DivideByZeroException()
+    {
+        int dividend = 10;
+        int divisor = 0;
+
+        Exception exception = Record.Exception(() => Division.Divide(dividend, divisor));
+
+        _ = Assert.IsType<DivideByZeroException>(exception);
+    }
+
+    [Theory]
+    [InlineData(int.MaxValue, int.MinValue, -0.999999999534)]
+    [InlineData(-int.MaxValue, int.MinValue, 0.999999999534)]
+    [InlineData(int.MinValue, int.MaxValue, -1.000000000466)]
+    [InlineData(int.MinValue, -int.MaxValue, 1.000000000466)]
+    public void Divide_ExtremeInput_CorrectCalculation(int dividend, int divisor, decimal expectedQuotient)
+    {
+
+        decimal actualQuotient = Division.Divide(dividend, divisor);
+
+        Assert.Equal(expectedQuotient, actualQuotient, 12);
+    }
+}
