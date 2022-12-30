493cf791d56c13c9b9a186af8e97385e99b6e346
Barry Wallis <barry.wallis@outlook.com>
2022-12-29 22:18:44 -0800

    Chapter 5 completed.

---
+++ TddByExample/Uqs.Customer.Tests.Unit/ProfileServiceTests.cs
@@ -0,0 +1,70 @@
+﻿namespace Uqs.Customer.Tests.Unit;
+
+public class ProfileServiceTests
+{
+    [Fact]
+    public void ChangeUsername_NullUsername_ArgumentNullException()
+    {
+        // Arrange
+        ProfileService sut = new();
+
+        // Act
+        Exception e = Record.Exception(() => sut.ChangeUsername(null!));
+
+        // Assert
+        ArgumentNullException ex = Assert.IsType<ArgumentNullException>(e);
+        Assert.Equal("username", ex.ParamName);
+    }
+
+    [Theory]
+    [InlineData("AnameOf8", true)]
+    [InlineData("NameOfChar12", true)]
+    [InlineData("AnameofChar13", false)]
+    [InlineData("NameOf7", false)]
+    [InlineData("", false)]
+    public void ChangeUsername_VariousLengthUsernames_ArgumentExceptionIfInvalid(string username, bool isValid)
+    {
+        // Arrange
+        ProfileService sut = new();
+
+        // Act
+        Exception e = Record.Exception(() => sut.ChangeUsername(username));
+
+        // Assert
+        if (isValid)
+        {
+            Assert.Null(e);
+        }
+        else
+        {
+            ArgumentException ex = Assert.IsType<ArgumentException>(e);
+            Assert.Equal("username", ex.ParamName);
+        }
+    }
+
+    [Theory]
+    [InlineData("Letter_123", true)]
+    [InlineData("!The_Start", false)]
+    [InlineData("InThe@Middle", false)]
+    [InlineData("WithDollar$", false)]
+    [InlineData("Space 123", false)]
+    public void ChangeUsername_InvalidCharValidation_ArgumentExceptionIfInvalid(string username, bool isValid)
+    {
+        // Arrange
+        ProfileService sut = new();
+
+        // Act
+        Exception e = Record.Exception(() => sut.ChangeUsername(username));
+
+        // Assert
+        if (isValid)
+        {
+            Assert.Null(e);
+        }
+        else
+        {
+            ArgumentException ex = Assert.IsType<ArgumentException>(e);
+            Assert.Equal("username", ex.ParamName);
+        }
+    }
+}
---
+++ TddByExample/Uqs.Customer/ProfileService.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Diagnostics;
+
+namespace Uqs.Customer;
+
+/// <summary>
+/// Provide services to manage user profiles.
+/// </summary>
+public class ProfileService
+{
+#pragma warning disable CA1822 // Mark members as static
+    /// <summary>
+    /// Change the user's name.
+    /// </summary>
+    /// <param name="username">The new user name.</param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="username"/> cannot be <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// <paramref name="username"/> must be between 8 and 12 characters inclusive.
+    /// </exception>
+    public void ChangeUsername(string username)
+    {
+        Guard.IsNotNull(username);
+        Guard.HasSizeGreaterThanOrEqualTo(username, 8);
+        Guard.HasSizeLessThanOrEqualTo(username, 12);
+        Guard.IsTrue(username.All(c => char.IsLetterOrDigit(c) || c == '_'), nameof(username));
+    }
+#pragma warning restore CA1822 // Mark members as static
+}
